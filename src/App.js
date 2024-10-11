12985e8d84b5f122787bad8b6460c26a6349881f
BartekMilew <bartosz.milew@gmail.com>
2024-10-11 11:26:45 +0200

    indexDB5

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       const getAllRequest = objectStore.getAll();
 
       getAllRequest.onsuccess = function (event) {
-        alert(JSON.stringify(event.target.result), indexDB);
+        alert(`${JSON.stringify(event.target.result)},dbIndex:${dbIndex}`);
         console.log(event.target.result);
       };
 
